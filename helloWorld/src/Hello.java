66ab6f09372276fda0bdde29dbef3c54cedf7298
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-23 10:27:58 -0500

    section 4 lecture 19

--- helloWorld/src/Hello.java
+++ helloWorld/src/Hello.java
@@ -1,10 +1,19 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Keith!");
-        int myFirstNumber = 5;
+        int myFirstNumber = 5 + 17 - 3;
         System.out.println(myFirstNumber);
         myFirstNumber = 12;
         System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+        System.out.println(myFirstNumber + 45);
+        System.out.println(myFirstNumber + "kittens");
+        int mySecondNumber = 45;
+        int myThirdNumber = 23;
+        int total = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(total);
+        int diff = 1000 - total;
+        System.out.println(diff);
     }
 }
 
