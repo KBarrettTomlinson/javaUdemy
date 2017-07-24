b7761e0e898669017aa52f5c321e384aa0b58644
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-24 14:47:27 -0500

    section 5 lecture 27

---
+++ statementsWhitespaceAndIndentation/src/com/kbtomlinson/Main.java
@@ -0,0 +1,27 @@
+package com.kbtomlinson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // STATEMENTS WHITESPACE AND INDENTATION
+
+        int myVariable = 50;
+        // statement is the whole line
+
+        System.out.println("This is a test");
+        // statement is the whole line
+
+        // a semi colon is typically an indication that you've completed a statement.
+
+        int anotherVariable = 50; myVariable --;
+        // above is valid and is two statements on one line but isn't recommended because of readability
+
+        // WHITESPACE
+        // INDENTATION
+
+        // IntelliJ under CODE menu Reformat Code
+
+    }
+}
