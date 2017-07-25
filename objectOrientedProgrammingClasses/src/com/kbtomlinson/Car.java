4e8cb94bdb83f76eef2365ab9ad286155e7d22a7
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-25 15:49:46 -0500

    Section 7 Lecture 37

---
+++ objectOrientedProgrammingClasses/src/com/kbtomlinson/Car.java
@@ -0,0 +1,27 @@
+package com.kbtomlinson;
+
+
+// STATEMENT TO CREATE A NEW CLASS CALLED CAR
+
+// Public is an access modifier. Public, Private, Protected.
+public class Car {
+
+    // variables
+    // local variable - local to or belong to a method or code block
+    // classes allow us to create variable which are visible to the object
+    // class, member variables are known as fields.
+
+    // encapsulation means that all of our fields will be private to the class
+
+    // creating fields for our class Car
+    // state or characteristic componants
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+
+    // creating a method for Car class
+    
+}
--- objectOrientedProgrammingClasses/src/com/kbtomlinson/Main.java
+++ objectOrientedProgrammingClasses/src/com/kbtomlinson/Main.java
@@ -1,8 +1,28 @@
 package com.kbtomlinson;
 
+// THIS IS A STATEMENT WHICH CREATES A NEW CLASS IN JAVA
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Object Orientated Programming - CLASSES
+
+        // objects have
+        //  state
+        //  behavior
+
+        // Class is template for creating an object
+
+        // Basic/Primitive data types are limited
+        // Classes are similar to a custom, user defined data type
+
+        // Instantiating an object based on a class
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        // All defined classes inherit certain methods from the java class
+
+
     }
 }
+
+
