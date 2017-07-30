46c18ed55bddbf9fa3c0ee0dc6c79965d1f4a57e
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-30 12:42:54 -0500

    Section 8 Lecture 46

--- objectOrientedProgrammingComposition/src/com/kbtomlinson/Main.java
+++ objectOrientedProgrammingComposition/src/com/kbtomlinson/Main.java
@@ -3,6 +3,26 @@ package com.kbtomlinson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// COMPOSITION
+        // Has a relationship with as apposed to being a subset of
+        // A computer HAS a motherboard, keyboard, and monitor
+
+        // Composition is where you have another class as a property of a class.
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+
+        Monitor theMonitor = new Monitor("27 in Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+
+        thePC.getMonitor().drawPixelAt(1500,1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+
+
+
+
     }
 }
---
+++ objectOrientedProgrammingComposition/src/com/kbtomlinson/Vehicle.java
@@ -0,0 +1,13 @@
+package com.kbtomlinson;
+
+public class Vehicle {
+
+    // properties
+    private String name;
+
+    // constructors
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
