bb1b098a066428461cdd0f4040711b1ab94c5903
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-08-07 21:45:45 -0500

    Section 10 Lecture 76

---
+++ udemyLectures/innerClasses/src/com/timbuchalka/Gearbox.java
@@ -0,0 +1,80 @@
+package com.timbuchalka;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dev on 2/10/2015.
+ */
+public class Gearbox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+    private boolean clutchIsIn;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+
+        for(int i=0; i<maxGears; i++) {
+            addGear(i, i* 5.3);
+        }
+    }
+
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if((number >0) && (number <= maxGears)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear) {
+        if((newGear>=0 ) && (newGear <this.gears.size()) && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs) {
+        if(clutchIsIn) {
+            System.out.println("Scream!!!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+    // NESTED Class
+    // Non static
+    // Inner Class
+    // Build a class like you would any other class.
+
+    // A gear is not useful outside of the context of the gearbox.
+    // Instance of gear has access to all of the properties and methods of the upper class
+    // even those marked as private.
+    private class Gear {
+
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+        // this refers to the inner class not the upper class
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
+        }
+
+        public double driveSpeed(int revs) {
+            return revs *( this.ratio);
+        }
+    }
+}
---
+++ udemyLectures/innerClasses/src/com/timbuchalka/Main.java
@@ -0,0 +1,58 @@
+package com.timbuchalka;
+
+import java.util.Scanner;
+
+public class Main {
+
+    //Nested classes
+    // Static
+    // Non-static Inner Class
+    // Local Class Inner Class defined inside a scope block
+    // Anonymous Classs - no name
+
+    Gearbox newGearbox = new Gearbox(6);
+    Gearbox.Gear first = newGearbox.new Gear(2, 23);
+
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+    public static void main(String[] args) {
+
+//        class ClickListener implements Button.OnClickListener {
+//            public ClickListener() {
+//                System.out.println("I've been attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//
+//        btnPrint.setOnClickListener(new ClickListener());
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+    }
+
+    private static void listen() {
+        boolean quit = false;
+        while(!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+
+            }
+        }
+    }
+}
